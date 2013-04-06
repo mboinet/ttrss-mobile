b1078e75af5f4d9f969fa91fb5ccd69323d61e0c
mboinet <matthieu.boinet@gmail.com>
2013-04-06 22:29:52 +0200

    utils in a separate module

--- scripts/main.js
+++ scripts/main.js
@@ -24,8 +24,8 @@ requirejs.config({
   } //path
 });
 
-requirejs(['jquery','backbone','templates','conf','models'],
-  function($, Backbone, tpl){
+requirejs(['jquery','backbone','templates','conf','models','utils'],
+  function($, Backbone, tpl, conf, models, utils){
 
 
 
@@ -357,7 +357,7 @@ function defineViews(){
       var link = "#" + Backbone.history.fragment +
         "/art" + this.model.id;
 
-      var dateStr = updateTimeToString(this.model.get("updated"));
+      var dateStr = utils.updateTimeToString(this.model.get("updated"));
 
       var html;
       var catId = window.feedsModel.getCurrentCatId();
@@ -662,7 +662,7 @@ function defineViews(){
       if (! time){
         time = "loading...";
       } else {
-        time = updateTimeToString(time);
+        time = utils.updateTimeToString(time);
       }
 
       var updTxt = "Published: ";
@@ -699,7 +699,7 @@ function defineViews(){
         var article = this.model.get("content");
 
         // apply content filters
-        article = cleanArticle(article, this.model.get("link"));
+        article = utils.cleanArticle(article, this.model.get("link"));
 
         // display article
         $contentDiv.html(article);
@@ -1044,57 +1044,6 @@ function defineRouter(){
 
 
 /************* utilities ***********/
-// to check the start of a string
-if (typeof String.prototype.startsWith != 'function') {
-  String.prototype.startsWith = function (str){
-    return this.indexOf(str) == 0;
-  };
-}
-
-// clean up a dom object (article to display)
-function cleanArticle(content, domain){
-  var data = "<article>" + content + "</article>";
-  var $dom = $(data);
-
-  /* ARS Technica styles DIVs */
-  $dom.find('div').removeAttr('style');
-  
-  /* ARS technica bookmarks */
-  $dom.find('div.feedflare').remove();
-  
-  /* Feedburner images */
-  $dom.find('img[href~="feedburner"]').remove();
-  
-  
-  var $toClean = $dom.find('img,object,iframe');
-  $toClean.removeAttr('height');
-    
-  $toClean.each(
-    function(index, e){
-         
-      // if relativeURL, add domain
-      var src = $(e).attr('src');
-      if ($.mobile.path.isRelativeUrl(src)){
-        var newsrc = $.mobile.path.makeUrlAbsolute(
-          src,
-          domain
-        );
-        $(e).attr('src', newsrc);
-      }      
-    }
-  );
-
-  // make all links open in a new tab
-  $toClean = $dom.find('a');
-  $toClean.each(
-    function(index, e){
-      $(e).attr('target', '_blank');
-    }
-  );
-
-  return $dom;
-}
-
 
 function registerLoginPageActions(){
 
@@ -1154,46 +1103,6 @@ function registerLoginPageActions(){
   }); // login button
 }
 
-
-// it returns a valid formatted string
-// of the update time representation of Tiny Tiny RSS
-function updateTimeToString(time){
-  var date = new Date(time * 1000); 
-  var now = new Date(Date.now());
-
-  // date in YYYY-MM-DD
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-  var day = date.getDate();
-  day = (day < 10 ? "0" : "") + day;
-  var dateStr = year + "-" + month + "-" + day;
-
-  // time in HH:MM
-  var hour = date.getHours();
-  hour = (hour < 10 ? "0" : "") + hour;
-  var min = date.getMinutes();
-  min = (min < 10 ? "0" : "") + min;
-  var  timeStr = hour + ":" + min;
-
-  // now in YYYY-MM-DD
-  year = now.getFullYear();
-  month = now.getMonth() + 1;
-  month = (month < 10 ? "0" : "") + month;
-  day = now.getDate();
-  day = (day < 10 ? "0" : "") + day;
-  var nowStr = year + "-" + month + "-" + day;
-
-  // if today, puts the time of day
-  if (dateStr == nowStr){
-    // only time if it's today
-    dateStr = timeStr;
-  } else {
-    dateStr = dateStr + " " + timeStr;
-  }
-
-  return dateStr;
-}
 
 function localStorageSupport(){
 
