53b10dbf5856608c85fda8654f27b59702ba482a
mboinet <matthieu.boinet@gmail.com>
2013-04-18 11:37:18 +0200

    tarball creation in make script

--- make/make.sh
+++ make/make.sh
@@ -1,3 +1,6 @@
 
 node r.js -o app.build.js
 
+VERSION=`grep Version ../build/index.html | sed 's/^.*<p>Version : \(.*\)<\/p>.*$/\1/'`
+cd ../build && tar -jcvf ../dist/ttrss-mobile-$VERSION.tar.bz2 ./
+
