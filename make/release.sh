ebca4ecdb853145d69036bb7123c3a6fbbf3fa3b
mboinet <matthieu.boinet@gmail.com>
2013-04-27 10:42:06 +0200

    also add README & CHANGELOG in a release

--- make/release.sh
+++ make/release.sh
@@ -5,6 +5,8 @@ BUILDDIR="../build/"
 VERSION=`grep Version ${BUILDDIR}/index.html | sed 's/^.*<p>Version : \(.*\)<\/p>.*$/\1/'`
 
 rm -v ${BUILDDIR}scripts/conf.js ${BUILDDIR}build.txt && \
+  cp ../CHANGELOG.md ${BUILDDIR} && \
+  cp ../README.md ${BUILDDIR} && \
   cd ${BUILDDIR} && \
   tar -jcvf ../dist/ttrss-mobile-$VERSION.tar.bz2 ./
 
